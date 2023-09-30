a1c5ca341939df1331b4972fcf720459a3c7ac36
ansman58 <javanslem@gmail.com>
2023-09-30 21:40:24 +0100

    make updates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,16 +1,15 @@
-const VideoModel = require("../models/videoModel");
 const fs = require("fs");
 const Video = require("../models/videoModel");
 
 class VideoController {
   static async addVideo(req, res) {
     try {
-      console.log("filllleee", req.file);
+      console.log("file", req.file);
       const videoPath = req.file.path;
       const videoName = req.file.originalname;
 
       // Store video information in the database
-      const video = await VideoModel.create({
+      const video = await Video.create({
         name: videoName,
         path: videoPath,
         meta: JSON.stringify(req.file),
@@ -20,7 +19,9 @@ class VideoController {
       res.json({ message: "Video uploaded successfully", videoId: video.id });
     } catch (error) {
       console.error("Error uploading video:", error);
-      res.status(500).json({ error: "Error uploading video" });
+      res
+        .status(500)
+        .json({ error: "Error uploading video", msg: error.message });
     }
   }
 
@@ -37,27 +38,38 @@ class VideoController {
       }
 
       const videoPath = video.path;
-      const videoSize = fs.statSync(videoPath).size;
 
+      // Create a readable stream for the video file
+      const videoStream = fs.createReadStream(videoPath);
+
+      // Set the response headers
+      res.setHeader("Content-Type", "video/mp4");
+      res.setHeader("Accept-Ranges", "bytes");
+
+      // Check for the Range header
       const range = req.headers.range;
-      if (!range) {
-        res.status(400).send("Requires range header");
-        return;
+      if (range) {
+        const positions = range.replace(/bytes=/, "").split("-");
+        const start = parseInt(positions[0], 10);
+        const end = positions[1] ? parseInt(positions[1], 10) : undefined;
+
+        if (!isNaN(start) && (!isNaN(end) || end === undefined)) {
+          res.status(206); // Partial Content
+          const contentLength = end ? end - start + 1 : undefined;
+          if (contentLength !== undefined) {
+            res.setHeader("Content-Length", contentLength.toString());
+            res.setHeader(
+              "Content-Range",
+              `bytes ${start}-${end}/${video.size}`
+            );
+            videoStream.pipe(res);
+            return;
+          }
+        }
       }
 
-      const CHUNK_SIZE = 10 ** 6; // 1MB
-      const start = Number(range.replace(/\D/g, ""));
-      const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
-      const contentLength = end - start + 1;
-      const headers = {
-        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-        "Accept-Ranges": "bytes",
-        "Content-Length": contentLength,
-        "Content-Type": "video/mp4",
-      };
-      res.writeHead(206, headers);
-      const videoStream = fs.createReadStream(videoPath, { start, end });
-
+      // If no valid range is specified, serve the entire video
+      res.status(200); // OK
       videoStream.pipe(res);
     } catch (error) {
       console.error("Error streaming video:", error);
