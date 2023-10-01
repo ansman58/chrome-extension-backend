66cf5dc0fe5ee264c3c019b7e67d48e91540136c
ansman58 <javanslem@gmail.com>
2023-10-01 14:57:48 +0100

    disable alter property from db

--- src/config/database.js
+++ src/config/database.js
@@ -16,7 +16,7 @@ const sequelize = new Sequelize(
 (async () => {
   try {
     await sequelize.authenticate();
-    await sequelize.sync({ alter: true });
+    await sequelize.sync({ force: false });
     console.log("Database Connection has been established successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
