ecdd294e6be75402a1b4a1eb349a40bc8e050fcd
ansman58 <javanslem@gmail.com>
2023-10-01 22:50:57 +0100

    fix

--- src/config/database.js
+++ src/config/database.js
@@ -16,7 +16,7 @@ const sequelize = new Sequelize(
 (async () => {
   try {
     await sequelize.authenticate();
-    await sequelize.sync({ force: false });
+    await sequelize.sync({ alter: true });
     console.log("Database Connection has been established successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
