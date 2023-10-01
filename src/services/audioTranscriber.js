638516a1978335e0a9fa8ddb6de672e59779e8b6
ansman58 <javanslem@gmail.com>
2023-10-01 12:15:27 +0100

    transcribe video

--- src/services/audioTranscriber.js
+++ src/services/audioTranscriber.js
@@ -2,83 +2,66 @@ require("dotenv").config();
 const fs = require("fs");
 const { Deepgram } = require("@deepgram/sdk");
 const path = require("path");
-// const VideoModel = require("../models/videoModel");
-const Video = require("../models/videoModel")
+const Video = require("../models/videoModel");
 
 const audioTranscriber = async (req, res, next) => {
   // Your Deepgram API Key
   const deepgramApiKey = process.env.DEEPGRAM_API_KEY;
 
-  // Location of the file you want to transcribe. Should include filename and extension.
-  // Example of a local file: ../../Audio/life-moves-pretty-fast.wav
-  // Example of a remote file: https://static.deepgram.com/examples/interview_speech-analytics.wav
+  try {
+    const videoId = req.params.videoId;
+    console.log("videoId:", videoId);
 
-  const videoId = req.params.videoId;
-  console.log("videoId:", videoId);
+    const video = await Video.findByPk(videoId);
 
-  const video = await Video.findByPk(videoId);
-  console.log("video in converter", videoId);
+    console.log("video", video);
 
-  if (!video) {
-    return res
-      .status(404)
-      .send("Video not found. Please provide a valid video ID");
-  }
+    if (!video) {
+      return res
+        .status(404)
+        .send("Video not found. Please provide a valid video ID");
+    }
 
-  const file = video.audioFilePath;
+    const file = video.audioFilePath;
 
-  // Mimetype for the file you want to transcribe
-  // Only necessary if transcribing a local file
-  // Example: audio/wav
-  const mimetype = "audio/mp3";
+    // Mimetype for the file you want to transcribe
+    const mimetype = "audio/mp3";
 
-  // Initialize the Deepgram SDK
-  const deepgram = new Deepgram(deepgramApiKey);
+    // Initialize the Deepgram SDK
+    const deepgram = new Deepgram(deepgramApiKey);
 
-  // Check whether requested file is local or remote, and prepare accordingly
-  if (file.startsWith("http")) {
-    // File is remote
-    // Set the source
-    console.log("remmootee fille", file);
+    let source;
 
-    source = {
-      url: file,
-    };
-  } else {
-    console.log("locall fille", file);
-    // File is local
-    // Open the audio file
-    const audio = fs.readFileSync(file);
-    // const audio = fs.createReadStream(file);
-    console.log("localll audio", audio);
+    // Check whether requested file is local or remote, and prepare accordingly
+    if (file.startsWith("http")) {
+      // File is remote
+      source = {
+        url: file,
+      };
+    } else {
+      // File is local
+      const audio = fs.readFileSync(file);
+      source = {
+        buffer: audio,
+        mimetype: mimetype,
+      };
+    }
 
-    // Set the source
-    source = {
-      buffer: audio,
-      mimetype: mimetype,
-    };
-  }
-
-  // Send the audio to Deepgram and get the response
-  deepgram.transcription
-    .preRecorded(source, {
+    // Send the audio to Deepgram and get the response
+    const response = await deepgram.transcription.preRecorded(source, {
       smart_format: true,
       model: "nova",
-    })
-    .then((response) => {
-      // Write the response to the console
-      //   console.dir(response, { depth: null });
-      console.log("Transcription successfulllllll");
-      // Write only the transcript to the console
-      console.dir(response.results.channels[0].alternatives[0].transcript, {
-        depth: null,
-      });
-      //   response.results.channels[0].alternatives[0].transcript;
-      next();
-    })
-    .catch((err) => {
-      console.log(err.message);
     });
+
+    // Write the transcript to the console
+    console.log("Transcription successful:");
+    console.log(response.results.channels[0].alternatives[0].transcript);
+
+    next();
+  } catch (err) {
+    console.error("Error:", err.message);
+    return res.status(500).send("An error occurred during transcription");
+  }
 };
 
 module.exports = audioTranscriber;
