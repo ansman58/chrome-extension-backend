3800845ee81d80ad8fde2ecdf3720bdb20c4c1df
ansman58 <javanslem@gmail.com>
2023-10-03 00:29:05 +0100

    remove trancribe endpoint from getVideo endpoint

--- src/routes/videos.js
+++ src/routes/videos.js
@@ -5,7 +5,7 @@ const audioTranscriber = require("../services/audioTranscriber");
 const router = express.Router();
 
 router.post("/", VideoController.addVideo);
-router.get("/:videoId", audioTranscriber, VideoController.getVideo);
+router.get("/:videoId", VideoController.getVideo);
 router.get("/", VideoController.getAllVideos);
 router.get(
   "/transcription/:videoId",
