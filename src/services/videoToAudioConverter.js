4fe43ef9eb5bea3e3201133808b4068ccebf5c30
ansman58 <javanslem@gmail.com>
2023-10-03 05:28:37 +0100

    fix ffmpeg issue

--- src/services/videoToAudioConverter.js
+++ src/services/videoToAudioConverter.js
@@ -1,11 +1,15 @@
 const fs = require("fs");
 const { exec } = require("child_process");
 const path = require("path");
+const VideoModel = require("../models/videoModel");
 
-function videoToAudioConverter(req, res, next) {
+async function videoToAudioConverter(req, res, next) {
   const targetDirectory = "public";
   const uploadSubdirectory = "public/audios";
-  const inputFile = req.file.path;
+
+  const video = await VideoModel.findByPk(req.params.videoId);
+
+  const inputFile = video.videoFilepath;
 
   const inputFileNameWithoutExtension = path.basename(
     inputFile,
@@ -16,6 +20,21 @@ function videoToAudioConverter(req, res, next) {
     uploadSubdirectory,
     inputFileNameWithoutExtension + ".mp3"
   );
+
+  const updateModel = await VideoModel.update(
+    {
+      audioFilePath: outputFile,
+    },
+    {
+      where: {
+        id: req.params.videoId,
+      },
+    }
+  );
+
+  if (!updateModel) {
+    return res.status(404).send("Error occured updating model");
+  }
 
   if (!fs.existsSync(targetDirectory)) {
     fs.mkdirSync(targetDirectory);
@@ -37,17 +56,15 @@ function videoToAudioConverter(req, res, next) {
         if (err) {
           console.error(`Download error: ${err.message}`);
           return next(err);
-        } 
-    
+        }
 
         // Cleanup: Delete the input and output files (uncomment if needed)
         // fs.unlinkSync(inputFile);
         // fs.unlinkSync(outputFile);
-
-        next();
       });
     }
   });
+  next();
 }
 
 module.exports = videoToAudioConverter;
